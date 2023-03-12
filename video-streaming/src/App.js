6a80befaa2eb7d53a5b9bd56fcd58ee2f01fafd6
Kunal G <kunalgursal2923@gmail.com>
2023-03-12 20:58:22 +0530

    bug fixed

--- video-streaming/src/App.js
+++ video-streaming/src/App.js
@@ -1,5 +1,4 @@
 import Home from './components/Home';
-import logo from './logo.svg';
 
 
 function App() {
--- video-streaming/src/components/Video.jsx
+++ video-streaming/src/components/Video.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import VideoJS from './VideoJS'
 import videojs from 'video.js';
